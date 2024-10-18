f92e28e0dabc82aa50ac17a0202db3f323951445
mconcoba <mconcoba20@gmail.com>
2024-10-18 01:05:46 -0600

    add: ✨config to use modal of component with boostrap

--- src/app/modules/users/add-users/add-users.component.ts
+++ src/app/modules/users/add-users/add-users.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-add-users',
   standalone: true,
   imports: [],
   templateUrl: './add-users.component.html',
-  styleUrl: './add-users.component.scss'
+  styleUrl: './add-users.component.scss',
 })
 export class AddUsersComponent {
+  constructor(private modalService: NgbModal) {}
 
+  closeModal() {
+    this.modalService.dismissAll();
+  }
 }
--- src/app/modules/users/list-users/list-users.component.ts
+++ src/app/modules/users/list-users/list-users.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AddUsersComponent } from '../add-users/add-users.component';
 
 @Component({
   selector: 'app-list-users',
   standalone: true,
   imports: [],
   templateUrl: './list-users.component.html',
-  styleUrl: './list-users.component.scss'
+  styleUrl: './list-users.component.scss',
 })
 export class ListUsersComponent {
+  constructor(private modalService: NgbModal) {}
 
+  openModal() {
+    const modalRef = this.modalService.open(AddUsersComponent, {
+      centered: true,
+      keyboard: false,
+    });
+  }
 }
