8ec8bad7329b0933d22f1421db88785e6a1105fb
mconcoba <mconcoba20@gmail.com>
2024-10-18 09:35:49 -0600

    add: ✨ implements alert service in btn deled user

--- src/app/modules/users/list-users/list-users.component.ts
+++ src/app/modules/users/list-users/list-users.component.ts
@@ -1,6 +1,8 @@
+import { AlertService } from './../../../services/alert.service';
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AddUsersComponent } from '../add-users/add-users.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-list-users',
@@ -10,12 +12,29 @@ import { AddUsersComponent } from '../add-users/add-users.component';
   styleUrl: './list-users.component.scss',
 })
 export class ListUsersComponent {
-  constructor(private modalService: NgbModal) {}
+  constructor(
+    private modalService: NgbModal,
+    private alertService: AlertService
+  ) {}
 
   openModal() {
     const modalRef = this.modalService.open(AddUsersComponent, {
       centered: true,
       keyboard: false,
     });
+  }
+
+  async deleteUser() {
+    const alertDeleted = await this.alertService.alertSimple(
+      '',
+      '¿Está seguro de eliminar el usuario?',
+      'question',
+      'Aceptar',
+      'Cancelar',
+      false
+    );
+    if (alertDeleted) {
+      console.log('usar api para eliminar');
+    }
   }
 }
