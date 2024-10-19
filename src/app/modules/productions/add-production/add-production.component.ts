2d1d4d636a9de8d7033acb99ced13d3730fa7bac
RuFlo167999 <ruddy16799@gmial.com>
2024-10-19 02:27:07 -0600

    add: ✨ button send action

--- src/app/modules/productions/add-production/add-production.component.ts
+++ src/app/modules/productions/add-production/add-production.component.ts
@@ -1,25 +1,94 @@
-import { Component, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, inject, NgModule, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import {
   NgLabelTemplateDirective,
   NgOptionTemplateDirective,
   NgSelectComponent,
 } from '@ng-select/ng-select';
+import { AlertService } from 'src/app/services/alert.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-production',
   standalone: true,
   imports: [
+    CommonModule,
     NgSelectComponent,
     NgLabelTemplateDirective,
     NgOptionTemplateDirective,
     FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './add-production.component.html',
   styleUrl: './add-production.component.scss',
 })
-export class AddProductionComponent {
+export class AddProductionComponent implements OnInit {
+  private _formBuilder = inject(FormBuilder);
   selectedCar: number;
+  production = this._formBuilder.group([
+    this.createProduction()
+  ]);
+
+  productionForm: FormGroup;
+
+  constructor(
+    private alertService: AlertService,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    console.log("Inicio del componenete")
+    this.generarFormularioProduccion()
+  }
+
+  createProduction(): FormGroup {
+    return this._formBuilder.group({
+      producto: [0, Validators.required],
+      encargado: [0, Validators.required],
+      fechaFin: ["", Validators.required],
+      fechaInicio: ["", Validators.required],
+      cantidad: [0, Validators.required]
+    });
+  }
+
+  generarFormularioProduccion(){
+    this.productionForm = this.fb.group({
+      producto: [0, Validators.required],
+      encargado: [0, Validators.required],
+      fechaFin: ["", Validators.required],
+      fechaInicio: ["", Validators.required],
+      cantidad: [0, Validators.required]
+    })
+  }
+  
+  async sendproduction() {
+    if (this.productionForm.invalid){
+      return
+    }
+
+    const nombreProducto= this.buscarElemento(this.cars, this.productionForm.value.producto)["name"]
+    const nombreEncargado= this.buscarElemento(this.cars2, this.productionForm.value.encargado)["name"]
+    const AlertService = await this.alertService.alertSimple(
+      'Confirmación de Envío',
+      //'¿Está seguro de que desea enviar la solicitud con los datos actuales?',
+      `<p> Producto: ${nombreProducto} </p> <p>Encargado: ${nombreEncargado}</p>
+      <p> Fecha Inicio: ${this.productionForm.value.fechaInicio} </p> <p> Fecha Fin: ${this.productionForm.value.fechaFin} </p>
+      <p> Cantidad a Fabricar: ${this.productionForm.value.cantidad} </p>`,
+      'warning',
+      'Sí, enviar',
+      'Cancelar',
+      false
+    );
+
+    if (AlertService) {
+      console.log('usar API para enviar');
+    }
+  }
+  
+  buscarElemento(listado: any[], valor: string){
+    return listado.find(item => item['id'] = valor)
+  }
 
   cars = [
     { id: 1, name: 'Camisa' },
@@ -34,4 +103,6 @@ export class AddProductionComponent {
     { id: 3, name: 'Encargado3' },
     { id: 4, name: 'Encargado4' },
   ];
+
+  
 }
