48c23c28eba7273dedd3527a2fac62ac39cf88b1
mconcoba <mconcoba20@gmail.com>
2024-10-21 01:37:22 -0600

    add: ✨ tbl componente global

---
+++ src/app/components/table/table.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FnData, TblInformation } from 'src/app/models/tbl-information.model';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.scss',
+})
+export class TableComponent implements OnInit {
+  @Input() tblInfo: TblInformation;
+  @Input() tblData: any[];
+  @Output() btnAction = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log(this.tblInfo);
+    console.log(this.tblData);
+  }
+
+  getNestedValue(obj: any, path: string) {
+    return path.split('.').reduce((acc, part) => acc && acc[part], obj);
+  }
+
+  async newEvent(data: FnData) {
+    this.btnAction.emit(data);
+  }
+}
