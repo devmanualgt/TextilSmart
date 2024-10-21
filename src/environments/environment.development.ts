daf43d382e10471deb89e91595ec78621da816a0
mconcoba <mconcoba20@gmail.com>
2024-10-21 01:38:57 -0600

    add: ✨ enviroments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,14 @@
+const url = {
+  protocol: 'http',
+  domain: 'localhost:3000',
+  path: 'api',
+};
+
+export const environment = {
+  production: false,
+  appVersion: 'v726demo1',
+  USERDATA_KEY: 'authf649fc9a5f55',
+  isMockEnabled: true,
+  API_URL: `${url.protocol}://${url.domain}/${url.path}`,
+  LIMIT_SIZE_FILES: 5 * 1024 * 1024,
+};
