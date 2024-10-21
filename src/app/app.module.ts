0bc2fa1f8891e7d8afd26094450a8fd9ef7fd490
mconcoba <mconcoba20@gmail.com>
2024-10-21 02:20:54 -0600

    add: ✨show splash screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { FilterPipe } from './pipe/filter.pipe';
 
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { SplashScreenModule } from './layouts/splash-screen/splash-screen.module';
 
 export function HttpLoaderFactory(http: HttpClient): any {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -49,6 +50,7 @@ export function HttpLoaderFactory(http: HttpClient): any {
     }),
     NgScrollbarModule,
     FullComponent,
+    SplashScreenModule,
   ],
   exports: [TablerIconsModule],
   bootstrap: [AppComponent],
