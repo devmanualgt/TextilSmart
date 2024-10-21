9a24227e32c0334970ab245d9880d1ed7eb702b4
mconcoba <mconcoba20@gmail.com>
2024-10-21 02:19:56 -0600

    add: ✨ componets to loader of requests

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,10 +5,11 @@ import { SearchComponent } from './table/search/search.component';
 import { MaterialModule } from '../material.module';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import { FormsModule } from '@angular/forms';
+import { LoaderComponent } from './loading/loader.component';
 
 @NgModule({
-  declarations: [TableComponent, SearchComponent],
+  declarations: [TableComponent, SearchComponent, LoaderComponent],
   imports: [CommonModule, MaterialModule, TablerIconsModule, FormsModule],
-  exports: [TableComponent, SearchComponent],
+  exports: [TableComponent, SearchComponent, LoaderComponent],
 })
 export class ComponentsModule {}
