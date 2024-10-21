c2261715ed1182c0349a3d1b05fb02306b691ecd
mconcoba <mconcoba20@gmail.com>
2024-10-21 10:41:08 -0600

    add: ✨ search value in tbl

--- src/app/components/table/search/search.component.ts
+++ src/app/components/table/search/search.component.ts
@@ -14,10 +14,14 @@ import {
 export class SearchComponent {
   @Input() textBtn: string;
   @Output() searchValue = new EventEmitter();
+  @Output() newValue = new EventEmitter();
 
   onSearch(event: any): void {
     const value = event.target.value;
     this.searchValue.emit(value);
-    // Aquí puedes realizar acciones como filtrar una lista
+  }
+
+  onNew(): void {
+    this.newValue.emit('click');
   }
 }
