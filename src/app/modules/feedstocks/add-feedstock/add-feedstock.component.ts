c755514cf61fcee7fa904a34d888dc92d9cf83ca
mconcoba <mconcoba20@gmail.com>
2024-10-21 12:36:05 -0600

    add: ✨ funtion to save feedstock

--- src/app/modules/feedstocks/add-feedstock/add-feedstock.component.ts
+++ src/app/modules/feedstocks/add-feedstock/add-feedstock.component.ts
@@ -1,17 +1,101 @@
-import { Component } from '@angular/core';
+import { AlertService } from './../../../services/alert.service';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FeedstockService } from './../services/feedstock.service';
+import { Component, OnInit } from '@angular/core';
 import { FloatLabelType } from '@angular/material/form-field';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { MaterialModule } from 'src/app/material.module';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-add-feedstock',
   standalone: true,
-  imports: [ComponentsModule, MaterialModule],
+  imports: [ComponentsModule, MaterialModule, FormsModule, ReactiveFormsModule],
   templateUrl: './add-feedstock.component.html',
   styleUrl: './add-feedstock.component.scss',
 })
-export class AddFeedstockComponent {
+export class AddFeedstockComponent implements OnInit {
   float: FloatLabelType = 'auto';
+  feedStockForm: FormGroup;
+  providers = [];
+  categories = [];
+  constructor(
+    private fb: FormBuilder,
+    public activeModal: NgbActiveModal,
+    private feedstockService: FeedstockService,
+    private alertService: AlertService
+  ) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    this.generateForm();
+  }
+
+  cerrarModal(refresh: boolean): void {
+    const closeInfo = { origin: 'feedstock', refresh };
+    this.activeModal.close(closeInfo);
+  }
+
+  async getProviders() {
+    const listProviders = await this.feedstockService.getProviders();
+    if (listProviders.status) {
+      this.providers = listProviders.data;
+    }
+  }
+
+  async getCategories() {
+    const listCategories = await this.feedstockService.getCategories();
+    if (listCategories.status) {
+      this.categories = listCategories.data;
+    }
+  }
+
+  generateForm() {
+    this.feedStockForm = this.fb.group({
+      nombre: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      unidadMedida: ['', Validators.required],
+      nivelMinimo: ['', Validators.required],
+      precioUnitario: ['', Validators.required],
+      proveedor: ['', Validators.required],
+      categoriaMateria: ['', Validators.required],
+    });
+    this.getProviders();
+    this.getCategories();
+  }
+
+  async sendFeedSock() {
+    if (this.feedStockForm.invalid) {
+      return;
+    }
+
+    await this.alertService.loader(
+      'Guardando...',
+      'Materia Prima se esta guardado',
+      0
+    );
+    const send = await this.feedstockService.postFeedStock(
+      this.feedStockForm.value
+    );
+    if (send.status) {
+      this.alertService
+        .alertSimple(
+          'Notificación',
+          send.message,
+          'success',
+          'Aceptar',
+          '',
+          true
+        )
+        .then(async (es) => {
+          this.cerrarModal(true);
+        });
+    }
+    console.log(this.feedStockForm.value);
+  }
 }
--- src/app/modules/feedstocks/services/feedstock.service.ts
+++ src/app/modules/feedstocks/services/feedstock.service.ts
@@ -1,3 +1,4 @@
+import { AlertService } from 'src/app/services/alert.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
@@ -8,7 +9,7 @@ import { environment } from 'src/environments/environment';
 })
 export class FeedstockService {
   API_URL = `${environment.API_URL}/v1`;
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private alertService: AlertService) {}
 
   async getFeedStock() {
     let msm;
@@ -26,6 +27,74 @@ export class FeedstockService {
       }
     } catch (error) {
       // this.handlerError.errorAlertNorm(error, msm);
+      return { status: false };
+    }
+  }
+
+  async getProviders() {
+    let msm;
+    try {
+      const response = await firstValueFrom(
+        this.http.get<any>(`${this.API_URL}/providers`, {
+          observe: 'response',
+        })
+      );
+
+      if (response?.ok) {
+        return { status: true, data: response.body['records'] };
+      } else {
+        return { status: false };
+      }
+    } catch (error) {
+      // this.handlerError.errorAlertNorm(error, msm);
+      return { status: false };
+    }
+  }
+
+  async getCategories() {
+    let msm;
+    try {
+      const response = await firstValueFrom(
+        this.http.get<any>(`${this.API_URL}/feedstocks/categories`, {
+          observe: 'response',
+        })
+      );
+
+      if (response?.ok) {
+        return { status: true, data: response.body['records'] };
+      } else {
+        return { status: false };
+      }
+    } catch (error) {
+      // this.handlerError.errorAlertNorm(error, msm);
+      return { status: false };
+    }
+  }
+
+  async postFeedStock(form: Object) {
+    let msm;
+    try {
+      const formData = await this.alertService.toUrlEncoded(form);
+      console.log(formData);
+
+      const response = await firstValueFrom(
+        this.http.post<any>(`${this.API_URL}/feedstocks`, formData, {
+          observe: 'response',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        })
+      );
+
+      if (response?.ok) {
+        return {
+          status: true,
+          data: response.body['records'],
+          message: response.body['message'],
+        };
+      } else {
+        return { status: false };
+      }
+    } catch (error) {
+      this.alertService.errorAlertNorm(error, error);
       return { status: false };
     }
   }
