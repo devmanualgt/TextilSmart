3731807797918d8a8e1240ea4b5175483dba80dd
mconcoba <mconcoba20@gmail.com>
2024-10-22 10:08:26 -0600

    add: ✨ requetes of get detail purchase order

--- src/app/modules/purchase-orders/detail-purchase-orders/detail-purchase-orders.component.ts
+++ src/app/modules/purchase-orders/detail-purchase-orders/detail-purchase-orders.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { InformationService } from './../../../services/information.service';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MaterialModule } from 'src/app/material.module';
+import { PurchaseOrderService } from '../services/purchase-order.service';
 
 @Component({
   selector: 'app-detail-purchase-orders',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './detail-purchase-orders.component.html',
-  styleUrl: './detail-purchase-orders.component.scss'
+  styleUrl: './detail-purchase-orders.component.scss',
 })
-export class DetailPurchaseOrdersComponent {
+export class DetailPurchaseOrdersComponent implements OnInit {
+  private route = inject(ActivatedRoute);
+  orderId!: string;
 
+  constructor(private purchaseOrderService: PurchaseOrderService) {}
+
+  ngOnInit(): void {
+    this.getDetailId();
+    this.getDetailOrder();
+  }
+
+  async getDetailOrder() {
+    const detail = await this.purchaseOrderService.findById(this.orderId);
+    console.log(detail);
+  }
+
+  getDetailId() {
+    this.route.paramMap.subscribe((params) => {
+      this.orderId = params.get('id')!;
+    });
+  }
 }
