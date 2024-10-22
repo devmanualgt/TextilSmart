c2b3e84a0ef9e03cd6df9dc5fba999464db34767
mconcoba <mconcoba20@gmail.com>
2024-10-22 02:13:23 -0600

    add: ✨ format of tbl list purchase orders

--- src/app/modules/purchase-orders/list-purchase-orders/list-purchase-orders.component.ts
+++ src/app/modules/purchase-orders/list-purchase-orders/list-purchase-orders.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { PurchaseOrderService } from './../services/purchase-order.service';
+import { Component, OnInit } from '@angular/core';
+import { MaterialModule } from 'src/app/material.module';
+import { TablerIconsModule } from 'angular-tabler-icons';
+import { FnData, TblInformation } from 'src/app/models/tbl-information.model';
+import { tbl_purchase_orders } from '../models/tbl-purchase-orders';
 
 @Component({
   selector: 'app-list-purchase-orders',
   standalone: true,
-  imports: [],
+  imports: [ComponentsModule, MaterialModule, TablerIconsModule],
   templateUrl: './list-purchase-orders.component.html',
-  styleUrl: './list-purchase-orders.component.scss'
+  styleUrl: './list-purchase-orders.component.scss',
 })
-export class ListPurchaseOrdersComponent {
+export class ListPurchaseOrdersComponent implements OnInit {
+  tlbInfo: TblInformation;
+  tblData: any;
+  constructor(private purchaseOrderService: PurchaseOrderService) {}
 
+  async ngOnInit() {
+    this.getOrders();
+  }
+
+  async getOrders() {
+    this.tlbInfo = {
+      tbl_name: 'Materia Prima',
+      export_xls: false,
+      scroll_tbl: false,
+      headers: tbl_purchase_orders.headers,
+      rows: tbl_purchase_orders.rows,
+      btns: tbl_purchase_orders.btn,
+    };
+    const getList = await this.purchaseOrderService.find();
+    if (getList.status) {
+      this.tblData = getList.data;
+    }
+  }
+
+  openModal(info?: FnData) {}
+
+  search(value: string) {
+    console.log(value);
+  }
+
+  actions(event: FnData) {}
 }
