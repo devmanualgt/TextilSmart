a2bf75ff087512ee3394f437977ffc0be277211f
mconcoba <mconcoba20@gmail.com>
2024-10-24 15:53:45 -0600

    add: ✨ envio de produccion

--- src/app/modules/productions/add-production/add-production.component.ts
+++ src/app/modules/productions/add-production/add-production.component.ts
@@ -1,17 +1,13 @@
 import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import {
-  Component,
-  inject,
-  NgModule,
-  OnChanges,
-  OnInit,
-  SimpleChanges,
-} from '@angular/core';
-import {
+  AbstractControl,
   FormBuilder,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 import {
@@ -23,17 +19,46 @@ import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { MaterialModule } from 'src/app/material.module';
 import { AlertService } from 'src/app/services/alert.service';
-import Swal from 'sweetalert2';
 import { FeedstockService } from '../../feedstocks/services/feedstock.service';
-import { MatNativeDateModule } from '@angular/material/core';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  MatNativeDateModule,
+} from '@angular/material/core';
 import { ProductService } from '../../products/services/product.service';
 import { ProductionService } from '../services/production.service';
 import { UserService } from '../../users/services/user.service';
 import { TblInformation } from 'src/app/models/tbl-information.model';
 import { tbl_producction_feedstock } from '../models/tbl-productions';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { AppDateAdapter } from 'src/app/shared/format-datepicker';
+import { NGX_MAT_DATE_FORMATS } from '@angular-material-components/datetime-picker';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { Router } from '@angular/router';
 
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'LL',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 @Component({
   selector: 'app-add-production',
+  providers: [
+    { provide: DateAdapter, useClass: AppDateAdapter },
+    //{ provide: MAT_DATE_FORMATS, useValue: MAT_DATE_FORMATS },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+    { provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } },
+  ],
   standalone: true,
   imports: [
     CommonModule,
@@ -47,6 +72,7 @@ import { tbl_producction_feedstock } from '../models/tbl-productions';
     ComponentsModule,
     MatNativeDateModule,
   ],
+
   templateUrl: './add-production.component.html',
   styleUrl: './add-production.component.scss',
 })
@@ -68,7 +94,8 @@ export class AddProductionComponent implements OnInit {
     private productService: ProductService,
     private productionService: ProductionService,
     private userService: UserService,
-    private alertService: AlertService
+    private alertService: AlertService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -88,7 +115,7 @@ export class AddProductionComponent implements OnInit {
   async getData() {
     try {
       // Mostrar loader
-      this.alertService.loader('Cargando...', 'Obteniendo datos...', 1000);
+      this.alertService.loader('Cargando...', 'Obteniendo datos...', 0);
 
       // Realizar todas las solicitudes en paralelo
       const [providersRes, productsRes, colorsRes, sizesRes, usersRes] =
@@ -116,7 +143,7 @@ export class AddProductionComponent implements OnInit {
         this.colors = colorsRes.data;
         this.sizes = sizesRes.data;
         this.users = usersRes.data;
-
+        this.alertService.close();
         console.log(this.sizes);
       } else {
         // Si alguna respuesta no fue exitosa, lanzar un error
@@ -134,14 +161,21 @@ export class AddProductionComponent implements OnInit {
 
   generarFormularioProduccion() {
     this.productionForm = this.fb.group({
-      proveedor: [null, Validators.required],
+      proveedor: [{ value: null, disabled: true }],
       producto: [null, Validators.required],
       color: [null, Validators.required],
       talla: [null, Validators.required],
       cantidad: [null, Validators.required],
       responsable: [null, Validators.required],
-      fechaFinEstimada: ['', Validators.required],
-      fechaInicio: ['', Validators.required],
+      fechaFinEstimada: [
+        '',
+        [Validators.required, this.fechaFinEstimadaValidator()],
+      ],
+      fechaInicio: [{ value: new Date(), disabled: true }, Validators.required],
+    });
+
+    this.productionForm.get('proveedor')?.valueChanges.subscribe(() => {
+      this.getValuesFeestock();
     });
 
     this.productionForm.get('producto')?.valueChanges.subscribe(() => {
@@ -157,6 +191,7 @@ export class AddProductionComponent implements OnInit {
     const producto = this.productionForm.get('producto')?.value;
     const cantidad = this.productionForm.get('cantidad')?.value;
     const proveedor = this.productionForm.get('proveedor')?.value;
+    console.log(proveedor);
 
     if (producto && cantidad && !proveedor) {
       const body = { producto, cantidad };
@@ -213,12 +248,69 @@ export class AddProductionComponent implements OnInit {
       false
     );
 
+    const dataFormSend = this.productionForm.value;
+    dataFormSend['fechaFinEstimada'] = this.formatDate(
+      dataFormSend.fechaFinEstimada
+    );
     if (AlertService) {
-      console.log('usar API para enviar');
+      const production = await this.productionService.create(dataFormSend);
+      if (production.status) {
+        this.alertService
+          .alertSimple(
+            'Notificación',
+            production.message,
+            'success',
+            'Aceptar',
+            '',
+            true
+          )
+          .then(async (es) => {
+            this.router.navigate([`productions/list`]);
+          });
+      }
     }
   }
 
   buscarElemento(listado: any[], valor: string) {
     return listado.find((item) => (item['id'] = valor));
   }
+
+  showProdiver(valor: any) {
+    console.log(valor);
+    const proveedorControl = this.productionForm.get('proveedor');
+
+    if (valor) {
+      proveedorControl?.enable(); // Habilita el control si valor es verdadero
+      proveedorControl?.addValidators(Validators.required);
+    } else {
+      proveedorControl?.setValue(null);
+      proveedorControl?.disable(); // Deshabilita el control si valor es falso
+    }
+  }
+
+  fechaFinEstimadaValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const currentDate = new Date();
+      const minDate = new Date(currentDate.setDate(currentDate.getDate() + 5));
+
+      const fechaFinEstimada = new Date(control.value);
+
+      if (fechaFinEstimada < minDate) {
+        return { fechaInvalida: true }; // Retorna un error si la fecha es menor que la fecha mínima
+      }
+      return null; // Retorna null si la fecha es válida
+    };
+  }
+
+  formatDate(date: any): string {
+    const fecha = new Date(date);
+    const day = ('0' + fecha.getDate()).slice(-2); // Obtener el día con dos dígitos
+    const month = ('0' + (fecha.getMonth() + 1)).slice(-2); // Obtener el mes con dos dígitos (sumar 1 porque los meses empiezan en 0)
+    const year = fecha.getFullYear(); // Obtener el año
+
+    return `${day}-${month}-${year}`; // Retornar en formato DD-MM-YYYY
+  }
+}
+function provideMomentDateAdapter(): import('@angular/core').Provider {
+  throw new Error('Function not implemented.');
 }
